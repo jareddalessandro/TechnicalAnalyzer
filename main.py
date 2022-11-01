9ebcec3b2c02fd3914de39245b4daa7d253a7aab
Jared Dalessandro <jareddalessandro1@gmail.com>
2022-11-01 00:53:48 -0700

    Added indicator wrapper functions - JD

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # TA-LIB Library https://github.com/mrjbq7/ta-lib
 # Use Finnhub for pricing queries and TA-Lib for technicals
 
-
 from os import error
 import finnhub
 import datetime
@@ -13,88 +12,124 @@ from email.mime.text import MIMEText
 import numpy as np
 from numpy import datetime64
 import pandas as pd
+import talib
+from talib import stream
 from talib.abstract import *
 
 finnhub_key = 'c6j6kjaad3ieecomvqh0'
 taapi_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjM1ZjE2YjJmYzVhOGFkZmVjZGM3MDIxIiwiaWF0IjoxNjY3MTc2MTE0LCJleHAiOjMzMTcxNjQwMTE0fQ.eqSZQiL9eCyfw7oFnaG5QsfVjpkFpWkRPgDFn8krksY'
-resolution = '1'
 symbol = "SPY"
 
 def main():
     now = int(time.time())
-    previous = (now - 1860) # 31 mins in the past, which will pull 30 entries
+    previous = (now - 12060) # 201 mins in the past, which will pull 200 entries
 
     now = 1667244600
-    previous = 1667242800
-
-
-    #print("Now:", now)
-    #print('Previous', previous)
+    previous = 1667232540 # 201 mins in the past, which will pull 200 entries
+        # Alert: This will likely need to be different so we get 200 entries for all time frames
 
     client = finnhub.Client(api_key=finnhub_key)
 
-    finnhub_candles = client.stock_candles(symbol=symbol, resolution=resolution, _from=previous, to=now)
-    candle_count = len(finnhub_candles['c']) # Get the count of how many candles we have
+    one_min_candles = client.stock_candles(symbol=symbol, resolution=1, _from=previous, to=now)
+    five_min_candles = client.stock_candles(symbol=symbol, resolution=5, _from=previous, to=now)
 
-    if finnhub_candles['s'] != 'ok':
+    candle_count = len(one_min_candles['c']) # Get the count of how many candles we have
+
+    if one_min_candles['s'] != 'ok' or five_min_candles['s'] != 'ok':
         print("ERROR: Houston we have a problem obtaining candle data from Finnhub.")
     else: 
         print("SUCCESS: Obtained candle data from Finnhub")
-    
-    # Digest the candle data into a list of dictionaries as required by TAAIO
-    #candles = [{}]
-    #for i in range(candle_count):
-    #   
-    #    dictionary = {
-    #            'open': finnhub_candles['o'][i],
-    #            'high': finnhub_candles['h'][i],
-    #            'low': finnhub_candles['l'][i],
-    #            'close': finnhub_candles['c'][i],
-    #            'volume': finnhub_candles['v'][i]
-    #        }
-    #    candles.append(dictionary)
-    #candles.pop(0) # remove the first entry which is an empty dictionary
-
-  
+ 
 
     # Create an np ndarray to be used by TA-LIB in OHLCV format
     one_min_data = {
-        'open': np.array(finnhub_candles['o']),
-        'high': np.array(finnhub_candles['h']),
-        'low': np.array(finnhub_candles['l']),
-        'close': np.array(finnhub_candles['c']),
-        'volume': np.array(finnhub_candles['v'])
+        'open': np.array(one_min_candles['o']),
+        'high': np.array(one_min_candles['h']),
+        'low': np.array(one_min_candles['l']),
+        'close': np.array(one_min_candles['c']),
+        'volume': np.array(one_min_candles['v'])
+    }
+    five_min_data = {
+        'open': np.array(five_min_candles['o']),
+        'high': np.array(five_min_candles['h']),
+        'low': np.array(five_min_candles['l']),
+        'close': np.array(five_min_candles['c']),
+        'volume': np.array(five_min_candles['v'])
     }
 
-    print(one_min_data)
+   
+    one_min_rsi = get_rsi(one_min_data)
+    five_min_rsi = get_rsi(five_min_data)
+
+    one_min_ma_200 = get_ma_200(one_min_data)
+    one_min_ma_120 = get_ma_120(one_min_data)
+    one_min_ma_50 = get_ma_50(one_min_data)
+
+    five_min_ma_200 = get_ma_200(five_min_data)
+    five_min_ma_120 = get_ma_120(five_min_data)
+    five_min_ma_50 = get_ma_50(five_min_data)
     
-    #np_array = np.array(finnhub_candles['c'])
-    #print(data)
-    #np_array = np.array(finnhub_candles)
-    # np_array = {'open': 'array'(finnhub_candles['o']), 'high': 'array'(finnhub_candles['h']), 'low': 'array'(finnhub_candles['l']), 'close': 'array'(finnhub_candles['c']), 'volume': tuple(finnhub_candles['v'])}
-    #print(np_array)
-
-
-    #SMA = abstract.SMA
-    #RSI = abstract.RSI
-
-    # This seems to be pretty accurate actually, it requires at least 14 increments of data
-    rsi_field = RSI(one_min_data, timeperiod=14)
-    ema_field = EMA(one_min_data)
-    current_rsi = rsi_field[len(rsi_field)-1]
+    one_min_ema_200 = get_ema_200(one_min_data)
+    one_min_ema_120 = get_ema_120(one_min_data)
+    one_min_ema_50 = get_ema_50(one_min_data)
+    one_min_ema_9 = get_ema_9(one_min_data)
     
-    print(ema_field)
-    print(current_rsi)
+    five_min_ema_200 = get_ema_200(five_min_data)
+    five_min_ema_120 = get_ema_120(five_min_data)
+    five_min_ema_50 = get_ema_50(five_min_data)
+    five_min_ema_9 = get_ema_9(five_min_data)
+
+    macd, macdsignal, macdhist = get_macd(one_min_data)
+    
+    print('\n')
+    print(macd, macdsignal, macdhist)
 
 
 
 
+############ INDICATOR Wrapper Functions ##############
+# Note: Don't use stream feature as it was returning same data for ema/ma/sma
 
 
+def get_rsi(data, period=14):
+    rsi_field = RSI(data, period)
+    return rsi_field[len(rsi_field)-1]
 
+def get_ema_9(data):
+    ema_9 = EMA(data['close'], 9)
+    return ema_9[len(ema_9)-1]
 
+def get_ema_50(data):
+    ema_50 = EMA(data['close'], 50)
+    return ema_50[len(ema_50)-1]
+    
+def get_ema_120(data):
+    ema_120 = EMA(data['close'], 120)
+    return ema_120[len(ema_120)-1]
 
+def get_ema_200(data):
+    ema_200 = EMA(data['close'], 200)
+    return ema_200[len(ema_200)-1]
 
+def get_ma_9(data):
+    ma_9 = MA(data['close'], 9)
+    return ema_9[len(ma_9)-1]
+
+def get_ma_50(data):
+    ma_50 = MA(data['close'], 50)
+    return ma_50[len(ma_50)-1]
+
+def get_ma_120(data):
+    ma_120 = MA(data['close'], 120)
+    return ma_120[len(ma_120)-1]
+
+def get_ma_200(data):
+    ma_200 = MA(data['close'], 200)
+    return ma_200[len(ma_200)-1]
+
+def get_macd(data):
+    macd, macdsignal, macdhist = MACD(data)
+    return round(macd[len(macd)-1], 4), round(macdsignal[len(macd)-1], 4), round(macdhist[len(macd)-1], 4)
 
 
 
